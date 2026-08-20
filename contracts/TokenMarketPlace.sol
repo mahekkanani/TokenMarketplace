7d1fb025d696cdfc93f7089ea5db36108ac790cf
mahekkanani <mahekkanani10@gmail.com>
2026-08-20 10:13:08 +0530

    erc20 token created initial state

--- contracts/TokenMarketPlace.sol
+++ contracts/TokenMarketPlace.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// IERC20 is an interface 
+
 contract TokenMarketPlace{
     //int for both positive and neg value store
     //uint for positive value store
@@ -10,9 +13,23 @@ contract TokenMarketPlace{
     uint private reservedOrderedTokens;
     //we can call only public variable cannot private variable
     // solidity shows the value of eth in wei 
+
+
+    IERC20 public gldToken;
+    
     error TokenMarketplace_ZeroTokens(uint256 numberOfTokens);
     error TokenMarketplace_InsufficientEthPayment(uint256 expectedPayment,uint256 actualPayment);
 
+    constructor(address _gldToken){
+        gldToken=IERC20(_gldToken);
+    }
+    //constructor executes first
+
+
+    //this function is used to fetch the balance of current wallet
+    function _getGldTokenBalanceMarketPlace() internal view returns(uint256){
+        return gldToken.balanceOf(address(this));
+    }
 
     function _isNumberOfTokensZero(uint256 numberOfTokens) internal pure { 
         if(numberOfTokens==0){
